8e185db3a854be72818bcbe6d08cb24e2f2111d2
RileyLanglois <rlanglois@wisc.edu>
2026-02-11 18:11:54 -0600

    added readme.md and comments on the code

--- .idea/lab1_wireless_code.py
+++ .idea/lab1_wireless_code.py
@@ -3,26 +3,30 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
-
+# extract pkl data
 file_path = sys.argv[1]
 
 with open(file_path, 'rb') as file:
     data = pickle.load(file)
 
-
 frames = data['frames']
+
+# declare necessary variables
 range_fft = 512
 azim_fft = 64
 max_range = 20
 
+# run the two ffts and shift it accordingly
 first_fft = np.fft.fft(frames, n = range_fft, axis = 2)
 second_fft = np.fft.fft(first_fft, n = azim_fft, axis = 1)
 second_fft = np.fft.fftshift(second_fft, axes = 1)
 
+# declare the axis labels
 second_axis_label = np.arccos(2*np.linspace(-0.5, 0.5, azim_fft)) / np.pi*180
 third_axis_label = np.linspace(0, 1, range_fft) * max_range
 X,Y = np.meshgrid(third_axis_label, second_axis_label)
 
+# plot the visualization
 fig = plt.figure()
 ax = plt.axes(projection = '3d')
 
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# lab1_CS839
-holds the contents necessary for lab1 of CS839
+# Instructions to Run lab1_wireless_code.py
+
+# Step 1
+Run 'pip install -r requirements.txt'
+
+# Step 2
+Run 'python lab1_wireless_code.py [inputfilename]'
+
+Make sure to replace 'inputfilename' with the path to the input pkl file to be read
+
+# Step 3
+Watch the visualization :)
